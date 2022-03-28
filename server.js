dbd26a8503404896ecd8a9f4ae96a042b3403ca6
saarthisharma <saarthi.sharma@dianapps.com>
2022-03-28 10:37:21 +0530

    mvc structure created

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const dotenv = require("dotenv")
+dotenv.config({path : ".env"})
+const PORT = process.env.PORT || 8080
+app.get("/" , (req,res)=>{
+    res.send("hey")
+})
+app.listen(PORT , ()=>{
+    console.log(`server is running at http://localhost:${PORT}`)
+})
+
