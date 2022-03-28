3f8840d307d30457ab91fa24483b7d7257a7dca0
saarthisharma <saarthi.sharma@dianapps.com>
2022-03-28 10:42:23 +0530

    mvc structure created

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv")
 dotenv.config({path : ".env"})
 const PORT = process.env.PORT || 8080
 app.get("/" , (req,res)=>{
-    res.send("hey")
+    res.send("request running")
 })
 app.listen(PORT , ()=>{
     console.log(`server is running at http://localhost:${PORT}`)
